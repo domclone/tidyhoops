4f1ad5b1558adf2b233c96a1464ee15efa3bc445
dom d'onofrio <domenic2692@gmail.com>
2021-01-04 20:17:16 -0500

    refactor getRequestUrl

--- scripts/bv-bet-history.js
+++ scripts/bv-bet-history.js
@@ -1,6 +1,7 @@
 const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
+const importJsonFromRestApi = require('../toolkits/data-extraction-toolkit/importJsonFromRestApi.js')
 const getRequestUrl = require('../toolkits/data-extraction-toolkit/getRequestUrl.js');
 
 const bvUrl = 'https://www.bovada.lv/sports/player-props?overlay=login'
@@ -13,5 +14,33 @@ const selectors = {
 };
 
 (async () => {
-  await getRequestUrl(bvUrl, bvBetHistoryRoute, credentials, selectors);
+  const requestUrl = await getRequestUrl(bvUrl, bvBetHistoryRoute, credentials, selectors);
+
+  requestUrl.searchParams.set('bet.isActive', '0');
+  requestUrl.searchParams.set('order', 'desc')
+  requestUrl.searchParams.delete('limit');
+
+  const cleanedUrl = requestUrl.href
+  const rawBets = await importJsonFromRestApi(cleanedUrl)
+
+  const cleanedBets = rawBets.data.map(entry => {
+    const bet = {
+      date: entry.events[0].games[0].date,
+      isParlay: entry.isAccumulator,
+      homeTeam: entry.events[0].games[0].homeTeam.abbreviation,
+      awayTeam: entry.events[0].games[0].visitingTeam.abbreviation,
+      playerTeam: entry.events[0].teams !== null ? entry.events[0].teams[0].abbreviation : null,
+      player: entry.events[0].players !== null ? entry.events[0].players[0].name : null,
+      setup: entry.events[0].statistic.title,
+      title: entry.title,
+      wager: entry.amount,
+      odds: entry.odds,
+      result: entry.events[0].settlement.result
+    }
+    return bet;
+  })
+  const straightBets = cleanedBets.filter(bet => bet.isParlay === 0); // we want to filter out parlays for now
+  console.log(straightBets);
+
+  return straightBets
 })();
--- toolkits/data-extraction-toolkit/getRequestUrl.js
+++ toolkits/data-extraction-toolkit/getRequestUrl.js
@@ -1,7 +1,5 @@
 const { chromium } = require('playwright');
 
-const importJsonFromRestApi = require('./importJsonFromRestApi.js')
-
 const getRequestUrl = async (url, route, credentials, selectors) => {
   const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
@@ -12,42 +10,12 @@ const getRequestUrl = async (url, route, credentials, selectors) => {
   await page.fill(selectors.password, credentials.password);
   await page.click(selectors.submit);
 
-  const interceptReq = async () => {
-    await page.route(route, async route => {
-      const requestUrl = new URL(route.request().url());
-
-      requestUrl.searchParams.set('bet.isActive', '0');
-      requestUrl.searchParams.set('order', 'desc')
-      requestUrl.searchParams.delete('limit');
-
-      const cleanedUrl = requestUrl.href
-      const rawBets = await importJsonFromRestApi(cleanedUrl)
-
-      const cleanedBets = rawBets.data.map(entry => {
-        const bet = {
-          date: entry.events[0].games[0].date,
-          isParlay: entry.isAccumulator,
-          homeTeam: entry.events[0].games[0].homeTeam.abbreviation,
-          awayTeam: entry.events[0].games[0].visitingTeam.abbreviation,
-          playerTeam: entry.events[0].teams !== null ? entry.events[0].teams[0].abbreviation : null,
-          player: entry.events[0].players !== null ? entry.events[0].players[0].name : null,
-          setup: entry.events[0].statistic.title,
-          title: entry.title,
-          wager: entry.amount,
-          odds: entry.odds,
-          result: entry.events[0].settlement.result
-        }
-        return bet;
-      })
-      const straightBets = cleanedBets.filter(bet => bet.isParlay === 0);
-      console.log(straightBets);
-
-    });
-  }
-  await interceptReq();
+  const request = await page.waitForRequest(route);
 
   await page.waitForNavigation(); // if this times out it is due to login not working
   await browser.close(); // without the above, the browser will close too early
+
+  return new URL(request.url());
 }
 
 module.exports = getRequestUrl;
