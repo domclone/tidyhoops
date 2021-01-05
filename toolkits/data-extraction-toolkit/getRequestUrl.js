de9af6c6c8dcaafdcdc49e0c4d835289963ab281
dom d'onofrio <domenic2692@gmail.com>
2021-01-05 16:45:29 -0500

    remove waitForNavigation

--- toolkits/data-extraction-toolkit/getRequestUrl.js
+++ toolkits/data-extraction-toolkit/getRequestUrl.js
@@ -5,14 +5,10 @@ const getRequestUrl = async (url, route, credentials, selectors) => {
   const page = await browser.newPage();
 
   await page.goto(url);
-
   await page.fill(selectors.email, credentials.username);
   await page.fill(selectors.password, credentials.password);
   await page.click(selectors.submit);
-
   const request = await page.waitForRequest(route);
-
-  await page.waitForNavigation(); // if this times out it is due to login not working
   await browser.close(); // without the above, the browser will close too early
 
   return new URL(request.url());
